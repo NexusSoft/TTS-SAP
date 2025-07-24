be3594a40f5abe9d602f831bf887d3a08d5c30b2
FranciscoAczayacatl <franciscoaczayacatlg@gmail.com>
2025-07-24 09:01:36 -0600

    Ejemplo json

--- SAP_TTS/Form1.cs
+++ SAP_TTS/Form1.cs
@@ -26,5 +26,50 @@ namespace SAP_TTS
             string json = JsonSerializer.Serialize(sample);
             File.WriteAllText("sample.json", json);
         }
+
+        private void BtnFormatoJSON_Click(object sender, EventArgs e)
+        {
+            var persona = new PersonaPrueba
+            {
+                Nombre = "Francisco",
+                Apellido = "Garcia",
+                DatosFiscales = new DatosFiscales
+                {
+                    RFC = "GACF800101X",
+                    Homoclave = "001"
+                },
+                Domicilio = new Domicilio
+                {
+                    Calle = "Avenida de las delicias",
+                    Colonia = "Jicalan",
+                    Numero = 100
+                    
+                }
+               
+
+            };
+            string json = JsonSerializer.Serialize(persona, new JsonSerializerOptions { WriteIndented = true });
+
+
+            richTextBox1.Text = json;
+        }
+    }
+    public class PersonaPrueba
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DatosFiscales DatosFiscales { get; set; }
+        public Domicilio Domicilio { get; set; }
+    }
+    public class DatosFiscales
+    {
+        public string RFC { get; set; }
+        public string Homoclave { get; set; }
+    }
+    public class Domicilio
+    {
+        public string Calle {  get; set; }
+        public string Colonia {  get; set; }
+        public int Numero {  get; set; }
     }
 }
